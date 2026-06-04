f9370da95dfd3fb6852028739f59c4726fd77b70
Chen0209 <2160514209qq.com>
2026-06-04 10:30:05 +0800

    chore：初始化仓库

---
+++ src/test/java/com/cc/novel_ai/NovelAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.novel_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
